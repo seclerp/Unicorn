b89a6a22301583647117961804bb1117292507bb
Andrew Rublyov <craftmaniack@gmail.com>
2019-09-25 23:31:08 +0300

    Start implementing parser, AST DU structure fixes

--- src/Unicorn/Models/AST.fs
+++ src/Unicorn/Models/AST.fs
@@ -4,12 +4,12 @@ type AST =
     | AST of TopLevelStatement list
 and TopLevelStatement =
     | Deps of string list
-    | Function of name: string * parameters: (string * string) list * returnType: string * body : Statement list
+    | Function of name: Id * parameters: (Id * Id) list * returnType: Id * body : Statement list
 and Statement =
     | ExpressionStatement of ExpressionStatement
     | Return of ExpressionStatement option
 and ExpressionStatement =
-    | VariableDeclaration of string * string * ExpressionStatement option
+    | VariableDeclaration of Id * Id * ExpressionStatement option
     | Assignment of l: string * r: ExpressionStatement
     | Expression of Expression
 and Expression =
@@ -17,11 +17,13 @@ and Expression =
     | Unary of Op * Expression
     | Primary of Primary
 and Primary =
-    | Id of string
+    | Id of Id
     | Character of char
     | Integer of int
-    | String of string
+    | String of String
     | Boolean of bool
+and Id = Id of string
+and String = String of string
 and Op =
     | Plus
     | Minus
--- src/Unicorn/Syntax/Parser.fs
+++ src/Unicorn/Syntax/Parser.fs
@@ -1,2 +1,151 @@
-module Unicorn.Syntax.Parser
+module rec Unicorn.Syntax.Parser
 
+open FParsec
+open Unicorn.Models.AST
+
+let private parsePlus = pchar '+' >>% Plus
+let private parseMinus = pchar '-' >>% Minus
+let private parseMult = pchar '*' >>% Mult
+let private parseDivide = pchar '/' >>% Divide
+
+let private pop =
+    spaces >>. choice [
+        parsePlus
+        parseMinus
+        parseMult
+        parseDivide
+    ]
+
+let private parseId =
+    spaces >>. many1Chars (satisfy isAsciiLetter) |>> Id
+let private parsePrimaryId =
+    parseId |>> Primary.Id
+
+let private parseCharacter =
+    spaces
+    >>. skipChar '\''
+    >>. anyChar
+    .>> skipChar '\''
+    |>> Character
+
+let private parseInteger = spaces >>. pint32 |>> Integer
+let private parseString =
+    spaces
+    >>. pchar '\"'
+    >>. manyChars anyChar
+    .>> pchar '\"'
+    |>> String
+let private parsePrimaryString =
+    parseString |>> Primary.String
+
+let private parseTrue = pstring "true" >>% Boolean true
+let private parseFalse = pstring "false" >>% Boolean false
+let private parseBoolean =
+    spaces >>. choice [
+        parseTrue
+        parseFalse
+    ]
+
+let private parsePrimary =
+    spaces >>. choice [
+        parsePrimaryId
+        parseCharacter
+        parseInteger
+        parsePrimaryString
+        parseTrue
+        parseFalse
+        parseBoolean
+    ]
+
+let private parseDeps =
+    let parseStringRaw =
+        spaces >>. (between (skipChar '"') (skipChar '"') (manyChars anyChar))
+    
+    parse {
+        do! spaces
+        do! skipString "deps"
+        do! spaces
+        do! skipChar '{'
+        do! spaces
+        let! deps = sepBy parseStringRaw (spaces >>. pchar ',' >>. spaces)
+        do! spaces
+        do! skipChar '}'
+        return Deps deps
+    }
+
+let private parseParameter =
+    parse {
+        let! name = parseId
+        do! spaces
+        do! skipChar ':'
+        do! spaces
+        let! type' = parseId
+        return (name, type')
+    }
+    
+let private parseParameters =
+    between
+        (pchar '(')
+        (pchar ')')
+        (spaces >>. (many parseParameter) .>> spaces)
+
+let private parseVariableDeclaration =
+    parse {
+        do! spaces
+        do! skipString "let"
+        do! spaces
+        let! name = parseId
+        do! spaces
+        do! skipChar ':'
+        do! spaces
+        let! type' = parseId
+        return VariableDeclaration (name, type')
+    }
+
+let private parseExpressionStatement =
+    spaces >>. choice [
+        parseVariableDeclaration
+        parseAssignment
+        parseExpression
+    ]
+
+let private parseReturn =
+    spaces >>. skipString "return" >>. spaces >>. parseExpressionStatement
+
+let private parseStatement =
+    spaces >>. choice [
+        parseExpressionStatement
+        parseReturn
+    ]
+
+let private parseFunctionBody =
+    spaces >>. many parseStatement
+
+let private parseFunction =
+    parse {
+        do! spaces
+        do! skipString "fun"
+        do! spaces
+        let! name = parseId
+        do! spaces
+        let! parameters = parseParameters
+        do! spaces
+        let! returnType = skipChar ':' >>. spaces >>. parseId
+        do! spaces
+        do! skipChar '{'
+        let! body = parseFunctionBody
+        do! spaces
+        do! skipChar '}'
+        return Function (name, parameters, returnType, body)
+    }
+
+let private parseTopLevelStatement =
+    spaces >>. choice [
+        parseDeps
+        parseFunction
+    ]
+
+let private parseProgram =
+    spaces
+    >>. many parseTopLevelStatement
+    >>. eof
